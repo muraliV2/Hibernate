295dcb2025b8ed18ea2d7be3a82933e01f8cddc4
Murali <muralisurya2004@gmail.com>
2025-12-24 22:23:54 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,74 +1,63 @@
-package get_set;
+package com.jspiders.hibernate.demo;
 
-public class student 
-{
-	
-private int rollno;
-private String name;
-private int age;
-private String contact;
+import java.io.Serializable;
 
-public student(int rollno,String name,int age,String contact)
-{
-	this.rollno = rollno;
-	this.name = name;
-	this.age = age;
-	this.contact = contact;
-}
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-public int getRollno() {
-	return rollno;
-}
-public void setRollno(int rollno) {
-	if(rollno>0)
-	{
-		this.rollno = rollno;
-	}
-	else
-	{
-		System.out.println("invalid data");
-	}
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) 
-{if(name.matches("[a-zA-z]+"))
+@Entity
+@Table(name = "student")
+public class Student 
 {
-	this.name = name;	
-}
-else
-{
-System.out.println("no special characters allowed");	
-}
-	
-}
-public int getAge() {
-	return age;
-}
-public void setAge(int age) 
-{
-	if(age>10)
-	{
-		this.age = age;
-	}
-	else
-	{
-		System.out.println("your age is too low");
-	}
-}
-public String getContact() {
-	return contact;
-}
-public void setContact(String contact) 
-{
-	if(contact.matches("[0-9]{10}"))
-	this.contact = contact;
-	else
-	{
-		System.out.println("plz provide needed credentials");
-	}
-}
 
+    @Id
+    private int rollno;
+    private String name;
+    private int age;
+    private String contact;
 
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+   
+    public Student() {}
+
+    public Student(int rollno, String name, int age, String contact) {
+        this.rollno = rollno;
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+    }
+
+    // getters & setters
 }
