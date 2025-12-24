507655c9d5cf668aa6f9878c54c7afb46e7ea820
Murali <muralisurya2004@gmail.com>
2025-12-24 22:24:28 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,15 @@
-package get_set;
+package com.jspiders.hibernate.demo;
+
+
+
 
 import java.sql.DriverManager;
 
+import com.jspiders.hibernate.demo.*;
+
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class Mainclas 
@@ -10,6 +17,19 @@ public class Mainclas
 public static void main(String [] args)
 {
 	EntityManagerFactory factory =Persistence.createEntityManagerFactory("development");
+	EntityManager manager = factory.createEntityManager();
+	EntityTransaction transaction = manager.getTransaction();
+	Student s1 = new Student();
+	s1.setRollno(1);
+	s1.setName("ramesh");
+	s1.setContact("1234567890");
+	s1.setAge(23);
+	transaction.begin();
+	manager.persist(s1);
+	transaction.commit();
+	
+	
+	
 	
 }
 }
