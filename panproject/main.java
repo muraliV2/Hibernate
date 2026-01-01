52eaa86d52a3a8d33daff0c3fd6f3691429935e1
Murali <muralisurya2004@gmail.com>
2026-01-01 22:51:30 +0530

    Create main.java

---
+++ panproject/main.java
@@ -0,0 +1,12 @@
+package com.hibernate.proj;
+
+import com.hibernate.dao.PanDao;
+import com.hibernate.dao.UserDao;
+
+public class App {
+    public static void main(String[] args) {
+
+        
+        PanDao.readdata(3);
+    }
+}
