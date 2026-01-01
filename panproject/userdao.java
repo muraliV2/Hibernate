26908c3bd7d1487006509487c82beaf2ce03b88e
Murali <muralisurya2004@gmail.com>
2026-01-02 01:44:48 +0530

    Update userdao.java

--- panproject/userdao.java
+++ panproject/userdao.java
@@ -1,52 +1,70 @@
 package com.hibernate.dao;
 
 import com.hibernate.dto.pancard;
-import com.hibernate.dto.user1;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-public class UserDao {
+public class PanDao {
 
     static EntityManagerFactory factory =
             Persistence.createEntityManagerFactory("development");
     static EntityManager manager = factory.createEntityManager();
     static EntityTransaction transaction = manager.getTransaction();
 
-    public static void insertdata(int userId, String name, String email,
-                                  long mobileNum, String dob, int panId) {
+    public static void insertdata(int panId, long panNo,
+                                  String panName, String gender, String dob) {
 
-        user1 user = new user1();
-        user.setUserId(userId);
-        user.setName(name);
-        user.setEmail(email);
-        user.setMobileNum(mobileNum);
-        user.setDob(dob);
-
-     
-        pancard pan = manager.find(pancard.class, panId);
-        user.setPancard(pan);
+        pancard pan = new pancard();
+        pan.setPanId(panId);
+        pan.setPanNo(panNo);
+        pan.setPanName(panName);
+        pan.setGender(gender);
+        pan.setDob(dob);
 
         transaction.begin();
-        manager.persist(user);
+        manager.persist(pan);
         transaction.commit();
     }
-public static void updatedata(int userId, String name, String email,
-                                  long mobileNum, String dob, int panId)
-{
-	
-user1 existingdata = manager.find(user1.class, userId);
-if(existingdata != null)
-{
-existingdata.setName(name);
-existingdata.setEmail(email);
-existingdata.setMobileNum(mobileNum);
-existingdata.setDob(dob);
-}
-else
-{
-	System.out.println("check the code for erroes");
-	}
-}
+    public static void updatedata(int panId, long panNo,
+                                  String panName, String gender, String dob)
+    {
+    	pancard existdata = manager.find(pancard.class, panId);
+    	if(existdata != null)
+    	{
+    	existdata.setPanNo(panNo);
+    	existdata.setPanName(panName);
+    	existdata.setGender(gender);
+    	existdata.setDob(dob);
+    	}
+    	else
+    	{
+    		System.out.println("check the error");
+    	}
+    	
+    }
+    public static void readdata(int PanId)
+    {
+    	pancard read = manager.find(pancard.class, PanId);
+    	if(read != null)
+    	{
+    		System.out.println(read.getPanId() + " " + read.getPanNo() + " " + read.getPanName() + " " + read.getGender() + " " + read.getDob());
+    		
+    	}
+    	else
+    	{
+    		System.out.println("check the code for any errors");
+    	}
+    	
+    }
+    
+    public static void deletedata(int PanId)
+    {
+    	pancard card = manager.find(pancard.class, PanId);
+    	transaction.begin();
+        manager.remove(card);
+        transaction.commit();
+    	
+    }
 }
