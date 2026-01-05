50c260a16dbcb15d120b9d0114932d7eca64b843
Murali <muralisurya2004@gmail.com>
2026-01-06 00:56:48 +0530

    Create app.java

---
+++ onetomany/app.java
@@ -0,0 +1,14 @@
+package com.ontomany.demo1;
+
+
+import com.ontomany.dao.companydao;
+import com.ontomany.dao.productdao;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+companydao.readdata(101);   			
+    }
+}
