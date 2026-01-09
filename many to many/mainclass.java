03fcff3c68c536437cb6437ef4f87f67f604c3bf
Murali <muralisurya2004@gmail.com>
2026-01-09 22:22:41 +0530

    Create mainclass.java

---
+++ many to many/mainclass.java
@@ -0,0 +1,26 @@
+package com.qsp.ontomany;
+
+import com.qsp.dao.departmentdao;
+import com.qsp.dao.employeedao;
+import com.qsp.dto.department;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	
+//    	employeedao.insertdata(1016, "smith", "IT","2023-06-10", 42000, 1001, 0, 10);
+//    	employeedao.insertdata(1017, "ALLEN", "ACCOUNTING", "2023-06-12", 38000, 1002, 0, 10);
+//    	employeedao.insertdata(1018, "WARD","ACCOUNTING", "2023-06-12", 40000,1003, 0, 10);
+//    	employeedao.insertdata(1019, "JONES","ACCOUNTING", "2023-06-14", 45000,1004, 0, 10);
+//    	employeedao.insertdata(1020, "MARTIN","RESEARCH", "2023-06-18", 36000,1005, 0, 20);
+//    	employeedao.insertdata(1021, "BLAKE","RESEARCH", "2023-06-20", 39000,1006, 0, 20);
+//    	employeedao.insertdata(1022, "CLARK","RESEARCH", "2023-06-22", 41000,1007, 0, 20);
+//    	employeedao.insertdata(1023, "SCOTT","RESEARCH", "2023-06-24", 43000,1008, 0, 20);
+    	employeedao.insertdata(1024, "KING","SALES", "2023-06-26", 50000,1009, 0, 30);
+    	
+    	
+        
+    }
+}
