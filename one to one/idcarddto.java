47acd5c5076308a851f4ed3b1d21ff952fdca0e7
Murali <muralisurya2004@gmail.com>
2026-01-11 00:39:23 +0530

    Update idcarddto.java

--- one to one/idcarddto.java
+++ one to one/idcarddto.java
@@ -1,5 +1,7 @@
 package com.hibernate.dto;
 
+import com.hibernate.dao.idcard;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -7,12 +9,48 @@ import jakarta.persistence.Persistence;
 
 public class idcarddto 
 {
+	static EntityManagerFactory factory = Persistence.createEntityManagerFactory("development");
+	static EntityManager manager = factory.createEntityManager();
+	static EntityTransaction transaction  = manager.getTransaction();
+
 public static void insertdata(int id,int cardnumber,String issuedate)
 {
-EntityManagerFactory factory = Persistence.createEntityManagerFactory("development");
-EntityManager manager = factory.createEntityManager();
-EntityTransaction transaction  = manager.getTransaction();
-
+idcard ic = new idcard();
+ic.setId(id);
+ic.setCardnumber(cardnumber);
+ic.setIssuedate(issuedate);
+transaction.begin();
+manager.persist(ic);
+transaction.commit();
+}
+public static void updatedata(int id,int cardnumber,String issuedate)
+{
+idcard existdata = manager.find(idcard.class,id);	
+if(existdata != null)
+{
+existdata.setCardnumber(cardnumber);
+existdata.setIssuedate(issuedate);
+transaction.begin();
+manager.persist(existdata);
+transaction.commit();
+}
+}
+public static void readdata(int id)
+{
+idcard ic = manager.find(idcard.class, id);
+if(ic != null)
+{
+System.out.println(ic.getId() + " " + ic.getCardnumber() + " " + ic.getIssuedate());	
+}
+transaction.begin();
+transaction.commit();
+}
+public static void delete(int id)
+{
+	idcard ic = manager.find(idcard.class, id);
+manager.remove(ic);	
+transaction.begin();
+transaction.commit();
 }
 
 
