8fb684c25520aabd7855011f0157a9e88c52905e
Murali <muralisurya2004@gmail.com>
2026-01-11 00:41:08 +0530

    Create mainclass.java

---
+++ one to one/mainclass.java
@@ -0,0 +1,23 @@
+package com.hibernate.onetone;
+
+import com.hibernate.dto.idcarddto;
+import com.hibernate.dto.studentdto;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+//        idcarddto.insertdata(1, 102, "20-12-2020");
+//        studentdto.insertdata(1, "22", "male", 1);
+//    	idcarddto.updatedata(1, 23, "23-jun-2019");
+//    	studentdto.updatedata(1, "24", "female", 1);
+//    	studentdto.readdata(1);
+//    	idcarddto.readdata(1);
+//    	idcarddto.insertdata(2, 202, "15-1-2017");
+//    	studentdto.insertdata(2, "20", "male", 2);
+//    	studentdto.delete(2);
+    	idcarddto.delete(2);
+    	
+    }
+}
