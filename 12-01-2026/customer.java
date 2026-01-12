af7ac389a5709fc638525feddd945e3c7315a822
Murali <muralisurya2004@gmail.com>
2026-01-12 23:28:10 +0530

    Create customer.java

---
+++ 12-01-2026/customer.java
@@ -0,0 +1,44 @@
+package com.hibernate.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+@Entity
+public class customer 
+{
+	@Id
+private int customerid;
+private String name;
+private String Email;
+private String Address;
+
+private List<order> orders = new ArrayList<order>();
+@OneToMany
+public int getCustomerid() {
+	return customerid;
+}
+public void setCustomerid(int customerid) {
+	this.customerid = customerid;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return Email;
+}
+public void setEmail(String email) {
+	Email = email;
+}
+public String getAddress() {
+	return Address;
+}
+public void setAddress(String address) {
+	Address = address;
+}
+}
