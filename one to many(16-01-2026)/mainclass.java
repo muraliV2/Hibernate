8348f44348b399a3c55ab2bae8aea126f545274e
Murali <muralisurya2004@gmail.com>
2026-01-16 16:27:57 +0530

    Create mainclass.java

---
+++ one to many(16-01-2026)/mainclass.java
@@ -0,0 +1,24 @@
+package com.hibernate.oneman;
+
+import com.hibernate.dto.doctordto;
+import com.hibernate.dto.hospitaldto;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+//    	hospitaldto.insertdata(102, "Fortis Hospital", "Bangalore");
+//    	hospitaldto.insertdata(103, "AIIMS", "New Delhi");
+//    	hospitaldto.insertdata(104, "KIMS Hospital", "Hyderabad");
+//    	hospitaldto.insertdata(105, "CMC Hospital", "Vellore");
+    	
+//    	     doctordto.insertdata(1, "Dr. Rahul", 35, "Male", 102);
+//    	    doctordto.insertdata(2, "Dr. Anjali", 30, "Female", 102);
+//    	    doctordto.insertdata(3, "Dr. Kumar", 40, "Male", 103);
+    	
+    	hospitaldto.readdata(102);
+    	
+
+    }
+}
