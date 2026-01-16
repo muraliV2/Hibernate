8bb170b28a2f4af284ca54114dced215ea715074
Murali <muralisurya2004@gmail.com>
2026-01-16 16:24:52 +0530

    Create hospital.java

---
+++ one to many(16-01-2026)/hospital.java
@@ -0,0 +1,39 @@
+package com.hibernate.dao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class doctors 
+{
+	@Id
+	private int doctorId;
+    private String doctorName;
+    private int age;
+    private String gender;
+    public int getDoctorId() {
+		return doctorId;
+	}
+	public void setDoctorId(int doctorId) {
+		this.doctorId = doctorId;
+	}
+	public String getDoctorName() {
+		return doctorName;
+	}
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
