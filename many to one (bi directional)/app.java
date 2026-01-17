0f9d0957dd0b33cfd3e1855d7e91f6c41f234d86
Murali <muralisurya2004@gmail.com>
2026-01-17 20:49:49 +0530

    Create app.java

---
+++ many to one (bi directional)/app.java
@@ -0,0 +1,28 @@
+package com.hibernate.manyne;
+
+import com.hibernate.dao.doctordao;
+import com.hibernate.dao.hospitaldao;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+//    	hospitaldao.insertdata(101, "Fortis Hospital", "Bangalore");
+//    	hospitaldao.insertdata(102, "AIIMS", "New Delhi");
+//    	hospitaldao.insertdata(103, "KIMS Hospital", "Hyderabad")
+
+//    	doctordao.insertdata(4, "Dr. Anil", 50, "Male", 101);
+//    	doctordao.insertdata(5, "Dr. Priya", 32, "Female", 101);
+//
+//    	doctordao.insertdata(6, "Dr. Kumar", 42, "Male", 102);
+//    	doctordao.insertdata(7, "Dr. Neha", 38, "Female", 102);
+//
+//    	doctordao.insertdata(8, "Dr. Ravi", 46, "Male", 103);
+//    	doctordao.insertdata(9, "Dr. Sneha", 34, "Female", 103);
+
+    	
+    	hospitaldao.read(101);
+    	
+    }
+}
