7db149fc9a302e5c49132dcbf5f0c3bf5bc0c59e
Murali <muralisurya2004@gmail.com>
2026-01-18 18:21:11 +0530

    Create main method.java

---
+++ many to many(18-01-2026)/main method.java
@@ -0,0 +1,22 @@
+package com.hibernate.manyman;
+
+import com.hibernate.dto.coursedto;
+import com.hibernate.dto.studentdto;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+//    	apStudentCourse(101, 1);
+//       coursedto. mapStudentCourse(101, 3);
+//       coursedto. mapStudentCourse(102, 2);
+//       coursedto.mapStudentCourse(103, 1);
+//       coursedto.mapStudentCourse(103, 2);
+//       coursedto.  mapStudentCourse(104, 3);
+//       coursedto.mapStudentCourse(105, 1);
+    	studentdto.readStudent(1);
+
+
+    }
+}
