6212e73d2f8af4cf822610171ba9ce1cf1645478
Murali <muralisurya2004@gmail.com>
2026-01-19 23:51:48 +0530

    Update app.java

--- many to one (bi directional)/app.java
+++ many to one (bi directional)/app.java
@@ -10,7 +10,6 @@ public class App {
     public static void main(String[] args) {
 //    	hospitaldao.insertdata(101, "Fortis Hospital", "Bangalore");
 //    	hospitaldao.insertdata(102, "AIIMS", "New Delhi");
-
 //    	doctordao.insertdata(8, "Dr. Ravi", 46, "Male", 103);
 //    	doctordao.insertdata(9, "Dr. Sneha", 34, "Female", 103);
 
