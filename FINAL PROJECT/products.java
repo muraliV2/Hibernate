08d195e15e55668d80d457ba4b5fa03dea0e028a
Murali <muralisurya2004@gmail.com>
2026-01-20 23:54:10 +0530

    Create products.java

---
+++ FINAL PROJECT/products.java
@@ -0,0 +1,31 @@
+package com.hibernate.dao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class products 
+{
+@Id
+private int productid;
+private String productname;
+public int getProductid() {
+	return productid;
+}
+public void setProductid(int productid) {
+	this.productid = productid;
+}
+public String getProductname() {
+	return productname;
+}
+public void setProductname(String productname) {
+	this.productname = productname;
+}
+public String getCategoryid() {
+	return categoryid;
+}
+public void setCategoryid(String categoryid) {
+	this.categoryid = categoryid;
+}
+private String categoryid;
+}
