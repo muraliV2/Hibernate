d78b7fe44b7c81d0c224a6c3015d869f3619d139
Murali <muralisurya2004@gmail.com>
2026-01-20 23:51:45 +0530

    Create customer.java

---
+++ project1/customer.java
@@ -0,0 +1,39 @@
+package com.hibernate.dao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class customer 
+{
+	@Id
+	private int customerid;
+	private String name;
+	private String email;
+	private String address;
+	public int getCustomerid() {
+		return customerid;
+	}
+	public void setCustomerid(int customerid) {
+		this.customerid = customerid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
