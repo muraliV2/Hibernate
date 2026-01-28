98a1d690300f3982d9807106f81c2e810e2c4c0d
Murali <muralisurya2004@gmail.com>
2026-01-29 04:03:28 +0530

    Create mainclass.java

---
+++ FINAL PROJECT/mainclass.java
@@ -0,0 +1,22 @@
+package com.hibernet.project;
+
+import com.hibernate.dto.customerdto;
+import com.hibernate.dto.deliverydto;
+import com.hibernate.dto.orderdto;
+import com.hibernate.dto.productdto;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	customerdto.insertdata(1,"Murali","murali@gmail.com","Chennai");
+    	deliverydto.insetdata(1,"Home Delivery","Pending");
+    	productdto.insertdata(101,"Laptop","Electronics");
+    	productdto.insertdata(102,"Mouse","Electronics");
+    	orderdto.insertdata(5001,"2026-01-29",1,1);
+    	orderdto.addproducttoorder(5001,101);
+    	orderdto.addproducttoorder(5001,102);
+
+    }
+}
