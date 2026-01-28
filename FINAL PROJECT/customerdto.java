1a15ac8a9cec0b4d29df7d2f3dd11aca79489250
Murali <muralisurya2004@gmail.com>
2026-01-29 04:02:13 +0530

    Create customerdto.java

---
+++ FINAL PROJECT/customerdto.java
@@ -0,0 +1,30 @@
+package com.hibernate.dto;
+
+import com.hibernate.dao.customer;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class customerdto 
+{
+ static EntityManagerFactory factory = Persistence.createEntityManagerFactory("development");
+ static EntityManager manager =  factory.createEntityManager();
+ static EntityTransaction transaction = manager.getTransaction();
+ 
+ public static void insertdata(int customerid,String name,String email,String address)
+ {
+	 transaction.begin();
+	 customer cust = new customer();
+	 cust.setCustomerid(customerid);
+	 cust.setName(name);
+	 cust.setEmail(email);
+	 cust.setAddress(address);
+	 
+	 manager.persist(cust);
+	 transaction.commit();
+	 
+	 
+ }
+}
