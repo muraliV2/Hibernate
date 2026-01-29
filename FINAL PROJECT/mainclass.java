b7981536fb1c95fce0562a97ed4c1ae2eb0d392b
Murali <muralisurya2004@gmail.com>
2026-01-30 00:28:40 +0530

    Update mainclass.java

--- FINAL PROJECT/mainclass.java
+++ FINAL PROJECT/mainclass.java
@@ -10,13 +10,14 @@ import com.hibernate.dto.productdto;
  */
 public class App {
     public static void main(String[] args) {
-    	customerdto.insertdata(1,"Murali","murali@gmail.com","Chennai");
-    	deliverydto.insetdata(1,"Home Delivery","Pending");
-    	productdto.insertdata(101,"Laptop","Electronics");
-    	productdto.insertdata(102,"Mouse","Electronics");
-    	orderdto.insertdata(5001,"2026-01-29",1,1);
-    	orderdto.addproducttoorder(5001,101);
-    	orderdto.addproducttoorder(5001,102);
+//    	customerdto.insertdata(1,"Murali","murali@gmail.com","Chennai");
+//    	deliverydto.insetdata(1,"Home Delivery","Pending");
+//    	productdto.insertdata(101,"Laptop","Electronics");
+//    	productdto.insertdata(102,"Mouse","Electronics");
+//    	orderdto.insertdata(5001,"2026-01-29",1,1);
+//    	orderdto.addproducttoorder(5001,101);
+//    	orderdto.addproducttoorder(5001,102);
 
+    	customerdto.getorder(1);
     }
 }
