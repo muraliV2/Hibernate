d08f6d569ba6594efa6678f310cca7891606bcfe
Murali <muralisurya2004@gmail.com>
2026-06-16 22:59:32 +0530

    Update app.java

--- many to one (bi directional)/app.java
+++ many to one (bi directional)/app.java
@@ -3,8 +3,7 @@ import com.hibernate.dao.doctordao;
 import com.hibernate.dao.hospitaldao;
 public class App {
     public static void main(String[] args) {
-//    	hospitaldao.insertdata(101, "Fortis Hospital", "Bangalore");
-//    	hospitaldao.insertdata(102, "AIIMS", "New Delhi");
+//    	hos
 
     	
     	hospitaldao.read(101);
