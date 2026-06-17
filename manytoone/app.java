46ef713ee77e1ff66b8fea0dec9c5b3f3250f8f6
Murali <muralisurya2004@gmail.com>
2026-06-17 22:59:53 +0530

    Update app.java

--- manytoone/app.java
+++ manytoone/app.java
@@ -4,7 +4,6 @@ import com.qsp.dao.departmentdao;
 import com.qsp.dao.employeedao;
 import com.qsp.dto.department;
 
-
 public class App {
     public static void main(String[] args) {
     	departmentdao.insertdata(40, "hotel", "chennai");
