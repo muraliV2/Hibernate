502e80799b813070712d09e263ddd51e9a5ffa08
Murali <muralisurya2004@gmail.com>
2026-06-22 22:11:50 +0530

    Update studentdto.java

--- many to many(18-01-2026)/studentdto.java
+++ many to many(18-01-2026)/studentdto.java
@@ -8,7 +8,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-
 public class studentdto 
 {
 static EntityManagerFactory factory  = Persistence.createEntityManagerFactory("development");
