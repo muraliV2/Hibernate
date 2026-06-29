fbddab47277e438f56a98a6a29eaae789369fd03
Murali <muralisurya2004@gmail.com>
2026-06-29 23:05:56 +0530

    Update app.java

--- bidirectional override/app.java
+++ bidirectional override/app.java
@@ -6,8 +6,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-public class App {
-	
+public class App {	
     public static void main(String [] args)
     { 	
     	AadharDao.finddata(2);
