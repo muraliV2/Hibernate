1052275532102fa4e68033465f14a1af307f938e
Murali <muralisurya2004@gmail.com>
2026-06-29 23:05:02 +0530

    Update app.java

--- bidirectional override/app.java
+++ bidirectional override/app.java
@@ -7,7 +7,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-
 public class App {
 	
 
